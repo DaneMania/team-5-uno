ad19aedfd924add2a05fb5d7a8e0b5b9dab23a81
FredrickoJerry <Corey.Vineyard@gmail.com>
2023-01-26 20:01:15 +0000

    Character Test

---
+++ LevelUpGame.Tests/levelup/CharacterTest.cs
@@ -0,0 +1,26 @@
+using levelup.cli;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace levelup
+{
+public class CharacterTest {
+    private Character? testObj;
+
+    string arbitraryName = "Arbitrary Name";
+
+    [SetUp]
+    public void SetUp()
+    {
+        testObj = new Character(arbitraryName);
+    }
+
+    [Test]
+        public void CharacterHasNameAndMoveCountWhenInitialized()
+        {
+            Assert.AreEqual(arbitraryName, testObj.Name);
+            Assert.AreEqual(0, testObj.moveCount);
+        }
+}
+}
+
---
+++ LevelUpGame/levelup/cli/Character.cs
@@ -0,0 +1,23 @@
+namespace levelup.cli;
+
+public class Character
+{  
+    private string _name;
+    private int count;
+    
+    static string DEFAULT_NAME= "Hazzah";
+
+    public Character() {
+        _name =DEFAULT_NAME;
+
+    }
+    public Character(string name) {
+        _name = name;
+    }
+    public string Name {
+        get{return _name;}
+    }
+    public int moveCount() {
+        return count;
+    }
+}
