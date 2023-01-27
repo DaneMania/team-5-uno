fc30d8a8534a64197831e5a400dd53fd53cc5a90
FredrickoJerry <Corey.Vineyard@gmail.com>
2023-01-27 15:49:57 +0000

    Updates 4

--- LevelUpGame.Tests/levelup/PositionTest.cs
+++ LevelUpGame.Tests/levelup/PositionTest.cs
@@ -5,12 +5,20 @@ using TechTalk.SpecFlow;
 namespace levelup {
     class PositionTest
     {
+        public Position pos=new Position(5,5);
+        
         public PositionTest()
         {
             Position position = new Position();
             position.X =  5;
         }
+    [Test]
+            public void ValidPositionTest()
+            {
+                Assert.AreEqual(0, pos.X);
+            }
     }
+
 }
 
 
